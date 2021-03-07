7a9d76672c4cfe3d22d98686d3c024ae2e180348
NiteshSukalikar <nitesh.sukalikar49@gmail.com>
2021-03-07 20:31:01 +0530

    end of Creating Insert Script and Readme File

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # EOSMachineTest
+
+# Before Run Api Project first you need to be sure to enter your Sql/Mysql ConnectionString in AppSettings.Json
---
+++ SQlScripts/Step3Scripts.sql
@@ -0,0 +1,7 @@
+insert into [dbo].[tblLogin] (Username,Password,Name,RoleId) values
+('Admin',123456,'Harry',1),('User',123456,'Kishor',2)
+
+
+insert into tblRole (id,Role) values(1,'Admin'),(2,'User')
+
+insert into tblStatus (id,Status) values(1,'Pending'),(2,'Approved'),(3,'Rejected')
